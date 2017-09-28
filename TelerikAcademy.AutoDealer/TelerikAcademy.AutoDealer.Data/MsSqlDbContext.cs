b26ddc2c2fbd77308d4e8476ab83bfb106bf6972
arushev <deso91@abv.bg>
2017-09-28 14:01:51 +0300

    data layer

---
+++ TelerikAcademy.AutoDealer/TelerikAcademy.AutoDealer.Data/MsSqlDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelerikAcademy.AutoDealer.Data
+{
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
