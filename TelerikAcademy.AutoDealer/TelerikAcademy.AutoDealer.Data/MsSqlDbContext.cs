658b0d53df11c5fb01d3de532c262caf42d1e189
arushev <deso91@abv.bg>
2017-09-29 14:54:27 +0300

    apply audit

--- TelerikAcademy.AutoDealer/TelerikAcademy.AutoDealer.Data/MsSqlDbContext.cs
+++ TelerikAcademy.AutoDealer/TelerikAcademy.AutoDealer.Data/MsSqlDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TelerikAcademy.AutoDealer.Data.Model;
+using TelerikAcademy.AutoDealer.Data.Model.Contracts;
 
 namespace TelerikAcademy.AutoDealer.Data
 {
@@ -13,6 +15,32 @@ namespace TelerikAcademy.AutoDealer.Data
         public MsSqlDbContext()
             : base("LocalConnection", throwIfV1Schema: false)
         {
+        }
+
+        public override int SaveChanges()
+        {
+            this.ApplyAuditInfoRules();
+            return base.SaveChanges();
+        }
+
+        private void ApplyAuditInfoRules()
+        {
+            foreach (var entry in
+                this.ChangeTracker.Entries()
+                    .Where(
+                        e =>
+                        e.Entity is IAuditable && ((e.State == EntityState.Added) || (e.State == EntityState.Modified))))
+            {
+                var entity = (IAuditable)entry.Entity;
+                if (entry.State == EntityState.Added && entity.CreatedOn == default(DateTime))
+                {
+                    entity.CreatedOn = DateTime.Now;
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.Now;
+                }
+            }
         }
 
         public static MsSqlDbContext Create()
