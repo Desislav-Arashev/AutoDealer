7eb3ed8cdb165d94552d65b18ca0e9ceee56d664
arushev <deso91@abv.bg>
2017-10-16 00:57:58 +0300

    home controller, car controller tests

---
+++ TelerikAcademy.AutoDealer/TelerikAcademy.AutoDealer.Web.Tests/Controllers/CarsControllerTest.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using TelerikAcademy.AutoDealer.Data.Model;
+using TelerikAcademy.AutoDealer.Services;
+using TelerikAcademy.AutoDealer.Web.App_Start;
+using TelerikAcademy.AutoDealer.Web.Controllers;
+using TelerikAcademy.AutoDealer.Web.Models;
+
+namespace TelerikAcademy.AutoDealer.Web.Tests.Controllers
+{
+    [TestClass]
+    public class CarsControllerTest
+    {
+        [TestMethod]
+        public void Contact_ShouldNotBeNull()
+        {
+            // Arrange
+            var carServiceMock = new Mock<ICarsService>();
+            var makesServiceMock = new Mock<IMakesService>();
+            var tranmissionsServiceMock = new Mock<ITransmissionsService>();
+            var mapperMock = new Mock<IMapper>();
+            CarsController controller = new CarsController(makesServiceMock.Object, tranmissionsServiceMock.Object, carServiceMock.Object,  mapperMock.Object);
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Search_ShouldNotBeNull()
+        {
+            // Arrange
+            var carServiceMock = new Mock<ICarsService>();
+            var makesServiceMock = new Mock<IMakesService>();
+            var tranmissionsServiceMock = new Mock<ITransmissionsService>();
+            var mapperMock = new Mock<IMapper>();
+            var mapper = new AutoMapperConfig();
+            Mapper.Initialize(cfg => {
+                cfg.CreateMap<Car, SliderViewModel>();
+            });
+            var viewModelMock = new Mock<SearchViewModel>();
+            CarsController controller = new CarsController(makesServiceMock.Object, tranmissionsServiceMock.Object, carServiceMock.Object, mapperMock.Object);
+
+            // Act
+            ViewResult result = controller.Search(viewModelMock.Object, 1) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void Details_ShouldNotBeNull()
+        {
+            // Arrange
+            var carServiceMock = new Mock<ICarsService>();
+            var makesServiceMock = new Mock<IMakesService>();
+            var tranmissionsServiceMock = new Mock<ITransmissionsService>();
+            var mapperMock = new Mock<IMapper>();
+            Mapper.Initialize(cfg => {
+                cfg.CreateMap<Car, DetailsViewModel>();
+            });
+            CarsController controller = new CarsController(makesServiceMock.Object, tranmissionsServiceMock.Object, carServiceMock.Object, mapperMock.Object);
+
+            // Act
+            ViewResult result = controller.Details(new Guid()) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        public void New_ShouldNotBeNull()
+        {
+            // Arrange
+            var carServiceMock = new Mock<ICarsService>();
+            var makesServiceMock = new Mock<IMakesService>();
+            var tranmissionsServiceMock = new Mock<ITransmissionsService>();
+            var carMock = new Mock<NewCarViewModel>();
+            var mapperMock = new Mock<IMapper>();
+            CarsController controller = new CarsController(makesServiceMock.Object, tranmissionsServiceMock.Object, carServiceMock.Object, mapperMock.Object);
+
+            // Act
+            ViewResult result = controller.New(carMock.Object) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
--- TelerikAcademy.AutoDealer/TelerikAcademy.AutoDealer.Web.Tests/Controllers/HomeControllerTest.cs
+++ TelerikAcademy.AutoDealer/TelerikAcademy.AutoDealer.Web.Tests/Controllers/HomeControllerTest.cs
@@ -2,53 +2,77 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using AutoMapper.QueryableExtensions;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TelerikAcademy.AutoDealer.Web;
 using TelerikAcademy.AutoDealer.Web.Controllers;
+using Moq;
+using TelerikAcademy.AutoDealer.Services;
+using TelerikAcademy.AutoDealer.Web.Models;
+using TelerikAcademy.AutoDealer.Data.Model;
+using AutoMapper;
+using TelerikAcademy.AutoDealer.Web.App_Start;
 
 namespace TelerikAcademy.AutoDealer.Web.Tests.Controllers
 {
     [TestClass]
     public class HomeControllerTest
     {
-        //[TestMethod]
-        //public void Index()
-        //{
-        //    // Arrange
-        //    HomeController controller = new HomeController();
+        [TestMethod]
+        public void Index_ShouldNotBeNull()
+        {
+            // Arrange
+            var carServiceMock = new Mock<ICarsService>();
+            var makesServiceMock = new Mock<IMakesService>();
+            var tranmissionsServiceMock = new Mock<ITransmissionsService>();
+            HomeController controller = new HomeController(carServiceMock.Object, makesServiceMock.Object, tranmissionsServiceMock.Object);
 
-        //    // Act
-        //    ViewResult result = controller.Index() as ViewResult;
+            // Act
+            ViewResult result = controller.About() as ViewResult;
 
-        //    // Assert
-        //    Assert.IsNotNull(result);
-        //}
+            // Assert
+            Assert.IsNotNull(result);
+        }
 
-        //[TestMethod]
-        //public void About()
-        //{
-        //    // Arrange
-        //    HomeController controller = new HomeController();
+        [TestMethod]
+        public void About_ShouldNotBeNull()
+        {
+            // Arrange
+            var carServiceMock = new Mock<ICarsService>();
+            var makesServiceMock = new Mock<IMakesService>();
+            var mapperMock = new Mock<IMapper>();
+            var list = new List<Car>();
+            IQueryable<Car> queryableCountries = list.AsQueryable();
+            var tranmissionsServiceMock = new Mock<ITransmissionsService>();
+            var mapper = new AutoMapperConfig();
+            Mapper.Initialize(cfg => {
+                cfg.CreateMap<Car, SliderViewModel>();
+            });
+            carServiceMock.Setup(x => x.GetAll()).Returns(queryableCountries);
+            HomeController controller = new HomeController(carServiceMock.Object, makesServiceMock.Object, tranmissionsServiceMock.Object);
 
-        //    // Act
-        //    ViewResult result = controller.About() as ViewResult;
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
 
-        //    // Assert
-        //    Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-        //}
+            // Assert
+            Assert.IsNotNull(result);
+        }
 
-        //[TestMethod]
-        //public void Contact()
-        //{
-        //    //// Arrange
-        //    //HomeController controller = new HomeController();
+        [TestMethod]
+        public void Contact_ShouldNotBeNull()
+        {
+            // Arrange
+            var carServiceMock = new Mock<ICarsService>();
+            var makesServiceMock = new Mock<IMakesService>();
+            var tranmissionsServiceMock = new Mock<ITransmissionsService>();
+            HomeController controller = new HomeController(carServiceMock.Object, makesServiceMock.Object, tranmissionsServiceMock.Object);
 
-        //    //// Act
-        //    //ViewResult result = controller.Contact() as ViewResult;
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
 
-        //    //// Assert
-        //    //Assert.IsNotNull(result);
-        //}
+            // Assert
+            Assert.IsNotNull(result);
+        }
     }
 }
